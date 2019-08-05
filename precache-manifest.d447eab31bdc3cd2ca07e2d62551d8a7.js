a8403122e104d4d25c83a37fdb8304fd8838c510
Lucero Dn <lucero_dn@hotmail.es>
2019-08-05 11:03:00 -0500

    Updates

---
+++ precache-manifest.d447eab31bdc3cd2ca07e2d62551d8a7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfee611c9aa70125080a0922f8e376e",
+    "url": "/FundacionBosch/index.html"
+  },
+  {
+    "revision": "418a8398467596c02ef7",
+    "url": "/FundacionBosch/static/css/2.d9a3577d.chunk.css"
+  },
+  {
+    "revision": "923739f522640258a7a9",
+    "url": "/FundacionBosch/static/css/main.0ad268a7.chunk.css"
+  },
+  {
+    "revision": "418a8398467596c02ef7",
+    "url": "/FundacionBosch/static/js/2.e3203ba4.chunk.js"
+  },
+  {
+    "revision": "923739f522640258a7a9",
+    "url": "/FundacionBosch/static/js/main.c1ac6ebd.chunk.js"
+  },
+  {
+    "revision": "df89da230f544cc31b11",
+    "url": "/FundacionBosch/static/js/runtime~main.e1974806.js"
+  },
+  {
+    "revision": "5cef7058730422524c4f20a2cf15cde3",
+    "url": "/FundacionBosch/static/media/Image 3@2x.5cef7058.png"
+  },
+  {
+    "revision": "e835d63328d249ded3654d3624ba2d01",
+    "url": "/FundacionBosch/static/media/becas.e835d633.jpg"
+  },
+  {
+    "revision": "6918da39aa799cf09604054f374474d8",
+    "url": "/FundacionBosch/static/media/imgFRB.6918da39.webp"
+  },
+  {
+    "revision": "77598c8bc06184056d3564cc5cb88a71",
+    "url": "/FundacionBosch/static/media/videoBosch.77598c8b.mp4"
+  }
+]);
